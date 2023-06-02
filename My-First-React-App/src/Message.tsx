29aabea47d8071a07ab55a6b3390e9b5762eeb27
nealwaga <waganeal@gmail.com>
2023-06-02 11:56:46 +0300

    created my first component

---
+++ My-First-React-App/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = "Neal";
+    if (name)
+        return <h1>Hello {name}!</h1>;
+    return <h1>Error</h1>;
+}
+
+export default Message;
