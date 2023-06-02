77fc96abe18d23aaeb14fac494cbed20adcf13be
nealwaga <waganeal@gmail.com>
2023-06-02 14:26:01 +0300

    handling events

--- My-First-React-App/src/components/ListGroup.tsx
+++ My-First-React-App/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let teams = [
     "Golden State Warriors",
@@ -6,18 +8,25 @@ function ListGroup() {
     "Portland Trail Blazers",
     "Boston Celtics",
   ];
-  teams = [];
+
+  // Event handler
+  const handleClick = (event:MouseEvent) => console.log(event)
 
   return (
     <>
       <h1>List</h1>
-      
-      {/* {teams.length == 0 ? <p>No teams found</p> : null} */}
+
       {teams.length == 0 && <p>No teams found</p>}
 
       <ul className="list-group">
-        {teams.map((team) => (
-          <li key={team}>{team}</li>
+        {teams.map((team, index) => (
+          <li
+            className="list-group-item"
+            key={team}
+            onClick={handleClick}
+          >
+            {team}
+          </li>
         ))}
       </ul>
     </>
