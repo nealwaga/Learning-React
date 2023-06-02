b57257579165a698142a6ffb962dbda86fb8dd85
nealwaga <waganeal@gmail.com>
2023-06-02 12:41:02 +0300

    rendering lists

--- My-First-React-App/src/components/ListGroup.tsx
+++ My-First-React-App/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
 function ListGroup() {
+  const teams = [
+    "Golden State Warriors",
+    "Miami Heat",
+    "Los Angeles Lakers",
+    "Portland Trail Blazers",
+    "Boston Celtics",
+  ];
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      <ul className="list-group">
+        {teams.map((team) => (
+          <li key={team}>{team}</li>
+        ))}
+      </ul>
     </>
   );
 }
