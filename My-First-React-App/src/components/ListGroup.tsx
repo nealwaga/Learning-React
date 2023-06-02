bea72e0a906a8b5eb4acfda7fae3a514d35623bb
nealwaga <waganeal@gmail.com>
2023-06-02 12:54:24 +0300

    conditional rendering

--- My-First-React-App/src/components/ListGroup.tsx
+++ My-First-React-App/src/components/ListGroup.tsx
@@ -1,15 +1,20 @@
 function ListGroup() {
-  const teams = [
+  let teams = [
     "Golden State Warriors",
     "Miami Heat",
     "Los Angeles Lakers",
     "Portland Trail Blazers",
     "Boston Celtics",
   ];
+  teams = [];
 
   return (
     <>
       <h1>List</h1>
+      
+      {/* {teams.length == 0 ? <p>No teams found</p> : null} */}
+      {teams.length == 0 && <p>No teams found</p>}
+
       <ul className="list-group">
         {teams.map((team) => (
           <li key={team}>{team}</li>
